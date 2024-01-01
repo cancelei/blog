e149072442178cb5e5b42bc0ede4457121e766d5
cancelei <glauber.bannwart@gmail.com>
2024-01-01 20:39:29 -0300

    Add user association to blog posts controller and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_29_215917) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_01_233703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_215917) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blog_posts_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_215917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blog_posts", "users"
   add_foreign_key "comments", "blog_posts"
 end
