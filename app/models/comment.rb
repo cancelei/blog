0973359d13e4f9f85ae00629103fd1ff50105b93
cancelei <glauber.bannwart@gmail.com>
2024-01-05 11:36:20 -0300

    Remove unused attributes and constructor from Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,3 @@
 class Comment < ApplicationRecord
-  attr_accessor :content, :author
   belongs_to :blog_post
-
-  def initialize(content, author)
-    @content = content
-    @author = author
-  end
 end
